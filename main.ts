082f7e49d5b44932abc226f8180613406da936b5
Brian Cable <brian@briancable.com>
2020-10-19 17:36:14 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    otherSprite.destroy()
+    music.baDing.play()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
+let direction = 0
+let mySprite2: Sprite = null
+let mySprite3: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . 3 3 3 . . . 
+    . . . . . . . . . . 3 3 3 . . . 
+    . . . . . . . . . . 3 . b . . . 
+    . . . . . . . . . . 3 . b . . . 
+    . . . . . . . . . . 3 . . . . . 
+    . . . . . . . . . . 3 . . . . . 
+    . . . . . . . 3 3 3 3 . . . . . 
+    . . . . . 3 3 3 3 3 3 . . . . . 
+    . . . . . . 3 3 3 3 3 . . . . . 
+    . . . . . . . . 3 3 3 . . . . . 
+    . . . . . . . . b . b . . . . . 
+    . . . . . . . . b . b . . . . . 
+    . . . . . . . b b . b b . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(10, 10)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+info.setScore(0)
+game.onUpdate(function () {
+    controller.moveSprite(mySprite, 100, 100)
+})
+game.onUpdateInterval(2000, function () {
+    mySprite3 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 7 7 . . . 7 . . . . . . 
+        . . . . 7 7 . . . 7 . . . . . . 
+        . . . . . 7 7 . . 7 . . 7 . . . 
+        . . . . . 7 7 . . 7 . . 7 . . . 
+        . . . . . 7 . . . 7 . . 7 . . . 
+        . . . . . 7 7 . 7 . . . 7 . . . 
+        . . . . . . 7 7 7 . . 7 7 . . . 
+        . . . . . . . 7 7 7 7 7 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    mySprite3.setPosition(randint(5, 155), randint(5, 115))
+})
+game.onUpdateInterval(1000, function () {
+    info.changeScoreBy(1)
+})
+game.onUpdateInterval(3000, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . 4 4 . . . . 
+        . . . . 5 . . . . 4 4 4 4 . . . 
+        . . . . 5 . . . . 4 5 5 4 . . . 
+        . . . . . 5 5 5 5 4 5 5 4 . . . 
+        . . . . 5 5 5 5 5 5 4 4 . . . . 
+        . . . . 5 . 5 5 5 5 5 . . . . . 
+        . . . . 5 . 5 . 5 . 5 . . . . . 
+        . . . . 5 . 5 . 5 . 5 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    mySprite2.setFlag(SpriteFlag.BounceOnWall, true)
+    direction = randint(1, 2)
+    if (direction == 1) {
+        mySprite2.setVelocity(-5, 20)
+    } else {
+        mySprite2.setVelocity(20, 5)
+    }
+})
